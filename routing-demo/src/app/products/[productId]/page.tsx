76618bbbfb7631cefe81fd461d2af76c37dcfaa5
karthikmanche <kartikmanche@gmail.com>
2025-12-25 20:32:15 +0530

    add updated

--- routing-demo/src/app/products/[productId]/page.tsx
+++ routing-demo/src/app/products/[productId]/page.tsx
@@ -1,3 +1,7 @@
-export default function ProducDetails(){
+export default async function ProducDetails({params,}:{
+  params:Promise<{productId:string}>
+
+}){
+  const productId=(await params).productId
   return <h1>details about product</h1>;
 }
