b93c51c19bda240cda63ad8832b137deaa16eb6c
karthikmanche <kartikmanche@gmail.com>
2025-12-25 19:42:32 +0530

    Fix nested git repositories

---
+++ routing-demo/src/app/about/page.tsx
@@ -0,0 +1,3 @@
+export default function About(){
+  return <h1>hello this is about me</h1>;
+}
---
+++ routing-demo/src/app/profile/page.tsx
@@ -0,0 +1,9 @@
+export default function Profile(){
+
+  <html>
+    <body>
+      
+    </body>
+  </html>
+  return <h1>this is profile page</h1>;
+}
